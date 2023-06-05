d3e2cd11e92afd6ba8e61374271c7b31d3d93745
Mor Zada Naveh <mor.zada23@gmail.com>
2023-06-05 12:04:44 +0300

    top nav responsiveness

---
+++ products.js
@@ -0,0 +1,21 @@
+const fs = require('node:fs');
+
+function myFunction() {
+    var topnav = document.getElementById("myTopnav");
+    if (topnav.className === "topnav") {
+        topnav.className += " responsive";
+    } else {
+        topnav.className = "topnav";
+    }
+  }
+
+// fs.writeFile("try.txt", "Hello from nodeJS!", (err) => {
+//     if (err) throw err;
+//     console.log("The file has been saved!")
+// })
+
+
+// fs.readFile("./try.txt", "utf8", (err, data) => {
+//     if (err) throw err;
+//     console.log(data)
+// })
