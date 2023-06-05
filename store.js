6860f726f9905b0ef62df00f301073ede61220a8
Mor Zada Naveh <mor.zada23@gmail.com>
2023-06-05 15:16:02 +0300

    store topnav responsive

---
+++ store.js
@@ -0,0 +1,8 @@
+function responsiveTopnav() {
+    var topnav = document.getElementById("myTopnav");
+    if (topnav.className === "topnav") {
+        topnav.className += " responsive";
+    } else {
+        topnav.className = "topnav";
+    }
+  }
